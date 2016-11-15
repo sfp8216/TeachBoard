652f82e6997756fe49209db6ffc007534b1cda28
Steven Pantaleo <Steven Pantaleo>
2016-11-15 16:37:19 -0500

    Working Tabs
    Might consider moving to material designed based app

--- app/src/main/java/com/rit/sfp/teachboard/MainActivity.java
+++ app/src/main/java/com/rit/sfp/teachboard/MainActivity.java
@@ -19,19 +19,19 @@ public class MainActivity extends AppCompatActivity {
         //Tab 1
         TabHost.TabSpec spec = host.newTabSpec("Tab One");
         spec.setContent(R.id.tab1);
-        spec.setIndicator("Chat");
+        spec.setIndicator("Board");
         host.addTab(spec);
 
         //Tab 2
         spec = host.newTabSpec("Tab Two");
         spec.setContent(R.id.tab2);
-        spec.setIndicator("Tools");
+        spec.setIndicator("Chat");
         host.addTab(spec);
 
         //Tab 3
         spec = host.newTabSpec("Tab Three");
         spec.setContent(R.id.tab3);
-        spec.setIndicator("Tab Three");
+        spec.setIndicator("Tools");
         host.addTab(spec);
     }
 
